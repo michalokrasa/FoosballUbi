31539a92c58e8a418f97ee65120d387d75668982
BalboBigguns <micholyea@gmail.com>
2020-04-03 19:09:58 +0200

    Fixes to previous commit

--- client/src/scenes/game.js
+++ client/src/scenes/game.js
@@ -34,7 +34,7 @@ export default class Game extends Phaser.Scene {
 
         let self = this;
 
-        if (process.env.CLOUD) {
+        if (process.env.NODE_ENV === 'CLOUD') {
             this.socket = io('http://ec2-3-126-55-4.eu-central-1.compute.amazonaws.com:3000');
         } else {
             this.socket = io('http://localhost:3000');
